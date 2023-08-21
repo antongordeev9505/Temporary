5dffcf69e13aa0d656a1f0795dbcf3aa37a314e1
Anton Gordeev <an.gordeev@andersenlab.com>
2023-08-22 00:48:32 +0300

    Configured build.gradle.kts files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,34 +4,9 @@ plugins {
 }
 
 android {
-    namespace = ConfigurationData.namespace
-    compileSdk = ConfigurationData.compileSdk
-
     defaultConfig {
         applicationId = ConfigurationData.applicationId
-        minSdk = ConfigurationData.minSdk
-        targetSdk = ConfigurationData.targetSdk
-        versionCode = ConfigurationData.versionCode
-        versionName = ConfigurationData.versionName
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                file("proguard-rules.pro")
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        namespace = ConfigurationData.namespace
     }
 }
 
@@ -47,4 +22,7 @@ dependencies {
     // Test
     testImplementation(Libraries.junit)
     androidTestImplementation(Libraries.junitTest)
+
+    // DI
+    implementation(Libraries.implementationDagger)
 }
