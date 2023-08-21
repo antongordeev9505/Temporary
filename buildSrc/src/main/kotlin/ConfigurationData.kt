ef590e46719fd1a335cac4dee24d4a33862b63f8
Anton Gordeev <an.gordeev@andersenlab.com>
2023-08-22 00:49:43 +0300

    Decreased minsdk to 24

--- buildSrc/src/main/kotlin/ConfigurationData.kt
+++ buildSrc/src/main/kotlin/ConfigurationData.kt
@@ -3,7 +3,7 @@ object ConfigurationData {
     const val namespace = "az.kapitalbank.birbankinvest"
     const val applicationId = "az.kapitalbank.birbankinvest"
     const val compileSdk = 33
-    const val minSdk = 26
+    const val minSdk = 24
     const val targetSdk = 33
     const val versionCode = 1
     const val versionName = "1.0"
